db06811a3e621f57ce66ab46e3666cb339a568d5
Felix <jose.ricardo.g.felix@accenture.com>
2023-09-18 20:23:54 -0300

    Ajustando código

--- src/TaskManagerController.java
+++ src/TaskManagerController.java
@@ -13,28 +13,58 @@ public class TaskManagerController {
         this.tasks = new ArrayList<Task>();
     }
 
-    public Integer createTask(String title, String description, String date, String priority) {
-        LocalDate dateTask = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    public String createTask(String title, String description, String date, String priority) throws Exception {
+        try {
+            String result;
 
-        Task task = new Task(title, description, dateTask, priority);
+            if (title == null || title == "") {
+                result = "O título informado é inválido!";
+            } else if (description == null) {
+                result = "A descrição informada é inválida!";
+            } else if (date == null || date == "") {
+                result = "A data de vencimento informada é inválida!";
+            } else if (priority == null || priority == "") {
+                result = "A prioridade informada é inválida!";
+            } else {
+                LocalDate dateTask = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 
-        if (!tasks.contains(task)) {
-            this.tasks.add(task);
-            return this.tasks.indexOf(task);
-        } else {
-            return -1;
+                if (dateTask.isBefore(LocalDate.now())) {
+                    result = "A data de vencimento informada já passou!";
+                } else if (priority != "Baixa" && priority != "Média" && priority != "Alta") {
+                    result = "A prioridade informada é inválida!";
+                } else {
+                    Task task = new Task(title, description, dateTask, priority);
+
+                    if (!this.tasks.contains(task)) {
+                        this.tasks.add(task);
+                        result = "Tarefa criada com sucesso!";
+                    } else {
+                        result = "Erro ao criar tarefa!";
+                    }
+                }
+
+            }
+
+            return result;
+        } catch (Exception e) {
+            throw new Exception(e);
         }
+
     }
 
     public String updateTitleTask(int i, String title) throws Exception {
         try {
-            String result = "Índice inválido";
+            String result;
 
-            if (i >= 0 && i < this.tasks.size()) {
+            if (title == null || title == "") {
+                result = "Erro ao atualizar título de tarefa!";
+            } else if (i >= 0 && i < this.tasks.size()) {
                 Task task = this.tasks.get(i);
                 task.setTitle(title);
 
-                result = task.getTitle();
+                result = "Título alterado com sucesso!";
+            } else {
+                result = "Índice inválido!";
             }
 
             return result;
@@ -45,13 +75,17 @@ public class TaskManagerController {
 
     public String updateDescriptionTask(int i, String description) throws Exception {
         try {
-            String result = "Índice inválido";
+            String result;
 
-            if (i >= 0 && i < this.tasks.size()) {
+            if (description == null) {
+                result = "Erro ao atualizar descrição de tarefa!";
+            } else if (i >= 0 && i < this.tasks.size()) {
                 Task task = this.tasks.get(i);
                 task.setDescription(description);
 
-                result = task.getDescription();
+                result = "Descrição alterada com sucesso!";
+            } else {
+                result = "Índice inválido!";
             }
 
             return result;
@@ -62,15 +96,26 @@ public class TaskManagerController {
 
     public String updateDateTask(int i, String date) throws Exception {
         try {
-            String result = "Índice inválido";
+            String result;
 
-            if (i >= 0 && i < this.tasks.size()) {
+
+            if (date == null || date == "") {
+                result = "Erro ao atualizar data de vencimento de tarefa!";
+            } else if (i >= 0 && i < this.tasks.size()) {
                 Task task = this.tasks.get(i);
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
                 LocalDate dateTask = LocalDate.parse(date, formatter);
 
-                task.setDate(dateTask);
-                result = task.getDate().format(formatter);
+                LocalDate today = LocalDate.now();
+
+                if (dateTask.isBefore(today)) {
+                    result = "A data de vencimento informada já passou!";
+                } else {
+                    task.setDate(dateTask);
+                    result = "Data de Vencimento alterada com sucesso!";
+                }
+            } else {
+                result = "Índice inválido!";
             }
 
             return result;
@@ -81,13 +126,17 @@ public class TaskManagerController {
 
     public String updatePriorityTask(int i, String priority) throws Exception {
         try {
-            String result = "Índice inválido";
+            String result;
 
-            if (i >= 0 && i < this.tasks.size()) {
+            if ((priority != "Baixa" && priority != "Média" && priority != "Alta") || priority == null || priority == "") {
+                result = "A prioridade informada é inválida!";
+            } else if (i >= 0 && i < this.tasks.size()) {
                 Task task = this.tasks.get(i);
                 task.setPriority(priority);
 
-                result = task.getPriority();
+                result = "Prioridade alterada com sucesso!";
+            } else {
+                result = "Índice inválido";
             }
 
             return result;
@@ -96,16 +145,18 @@ public class TaskManagerController {
         }
     }
 
-    public void removeTask(String title, String description, String date, String priority) throws Exception {
+    public String removeTask(int i) throws Exception {
         try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-            LocalDate dateTask = LocalDate.parse(date, formatter);
+            String result;
 
-            Task task = new Task(title, description, dateTask, priority);
-
-            if (this.tasks.contains(task)) {
-                this.tasks.remove(task);
+            if (i >= 0 && i < this.tasks.size()) {
+                this.tasks.remove(i);
+                result = "Tarefa removida com sucesso!";
+            } else {
+                result = "Índice inválido";
             }
+
+            return result;
         } catch (Exception e) {
             throw new Exception(e);
         }
@@ -123,7 +174,7 @@ public class TaskManagerController {
             Collections.sort(this.tasks, comparator);
 
             for (int i = 0; i < this.tasks.size(); i++) {
-                result += this.tasks.get(i) + "\n";
+                result += i+1 + " - " + this.tasks.get(i).toString() + "\n";
             }
 
             return result;
